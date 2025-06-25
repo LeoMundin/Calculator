fe9650a225695279df9910a3f8148eee98a8b787
leo <LM274971@falmouth.ac.uk>
2025-06-25 18:15:17 +0100

    Basic Functionality

--- SimpleCalculator/SimpleCalculator.cpp
+++ SimpleCalculator/SimpleCalculator.cpp
@@ -1,20 +1,51 @@
-// SimpleCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <format>
+
+using namespace std;
+
+char opperator;
+
+float firstValue;
+float secondValue;
+
+
+float EvaluateOperation()
+{
+    switch (opperator) {
+    case '+':
+        return firstValue + secondValue;
+       
+    case '-':
+        return firstValue - secondValue;
+        
+    case '*' || 'x' || 'X':
+        return firstValue * secondValue;
+    default:
+        cout << "Opperator not available ";
+    }
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+   
+    cout << "Input your first value : ";
+    cin >> firstValue;
+
+    cout << "input your opporator : ";
+    cin >> opperator;
+
+    cout << "input your second value : ";
+    cin >> secondValue;
+
+
+
+    cout << "Your answer is : " << EvaluateOperation();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
