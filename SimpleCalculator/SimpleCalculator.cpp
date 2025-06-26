ab61ad405ee7ea82397326be5a963c7f3770bb7d
leo <LM274971@falmouth.ac.uk>
2025-06-26 14:36:30 +0100

    Simple Calculator

--- SimpleCalculator/SimpleCalculator.cpp
+++ SimpleCalculator/SimpleCalculator.cpp
@@ -1,48 +1,86 @@
-
 #include <iostream>
-#include <format>
+#include <string>
+#include <limits>
 
 using namespace std;
 
-char opperator;
-
-float firstValue;
-float secondValue;
-
-
-float EvaluateOperation()
+/// <summary>
+/// Takes in two values and allows for simple addition, subtraction, multiplication and division of thoes two values.
+/// </summary>
+class Calculator
 {
-    switch (opperator) {
-    case '+':
-        return firstValue + secondValue;
-       
-    case '-':
-        return firstValue - secondValue;
-        
-    case '*' || 'x' || 'X':
-        return firstValue * secondValue;
-    default:
-        cout << "Opperator not available ";
-    }
 
-}
+    private:
 
+        char opperator;
+
+        float firstValue;
+        float secondValue;
+
+        void CatchInputFail()
+        {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Input failed try again \n";
+                Calculate();
+            }
+        }
+
+        // Evaluates the inputed values and applies the appropriate oprations.
+        float EvaluateOperation()
+        {
+            switch (opperator) {
+            case '+':
+                return firstValue + secondValue;
+
+            case '-':
+                return firstValue - secondValue;
+
+                // Checks multiple forms of input
+            case '*' || 'x' || 'X':
+                return firstValue * secondValue;
+
+            case '/':
+                return firstValue / secondValue;
+
+            default:
+                cout << "Opperator not available ";
+            }
+
+        }
+
+    public:
+
+        // Takes in values and outputs the result
+        void Calculate()
+        {
+            cout << "Input your first value : ";
+            cin >> firstValue;
+            CatchInputFail();
+
+            cout << "input your opporator : ";
+            cin >> opperator;
+            CatchInputFail();
+
+            cout << "input your second value : ";
+            cin >> secondValue;
+            CatchInputFail();
+
+            cout << "Your answer is : " << EvaluateOperation();
+
+        }
+
+};
+
+
+// Runs the code
 int main()
 {
 
-   
-    cout << "Input your first value : ";
-    cin >> firstValue;
-
-    cout << "input your opporator : ";
-    cin >> opperator;
-
-    cout << "input your second value : ";
-    cin >> secondValue;
-
-
-
-    cout << "Your answer is : " << EvaluateOperation();
+    // Creates an instance of the calculator and initiates the calculation process
+    Calculator calculator;
+    calculator.Calculate();
 
     return 0;
 }
